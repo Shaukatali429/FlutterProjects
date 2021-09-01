8fcfcb9393cfbba950c003e454399c14791a730c
Shaukatali429 <mominshaukatali@gmail.com>
2021-09-01 18:50:04 +0530

    loginapp completed

---
+++ login/lib/home.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  final int days = 30;
+  final String name = "shaukatali";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("catlog app"),
+      ),
+        body: Center(
+          child: Container(
+            child: Text("Welcome to $days days of flutter by $name"),
+          ),
+        ),
+        drawer: Drawer(),
+      );
+      
+    
+  }
+}
